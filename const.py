8c208b5d158b4302179d87f36328c167592e3f1b
Hekmon <edouardhur@gmail.com>
2022-07-02 22:36:50 +0200

    prepare constants

--- const.py
+++ const.py
@@ -1,4 +1,21 @@
 """Constants for the Linky (LiXee-TIC-DIN) integration."""
 
+import serial
+
 DOMAIN = "lixeeticdin"
 
+# Protocol configuration
+#   https://www.enedis.fr/media/2035/download
+
+BYTESIZE = serial.SEVENBITS
+PARITY = serial.PARITY_EVEN
+STOPBITS = serial.STOPBITS_ONE
+
+MODE_STANDARD_BAUD_RATE = 9600
+MODE_STANDARD_FIELD_SEPARATOR = b'\x09'
+
+MODE_HISTORIQUE_BAUD_RATE = 1200
+MODE_HISTORIQUE_FIELD_SEPARATOR = b'\x20'
+
+LINE_END = b'\r\n'
+FRAME_END = b'\r\x03\x02\n'

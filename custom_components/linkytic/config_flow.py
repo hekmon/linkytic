cd0fd4069e81fcd7f84068bb1a370279ddad680c
Tom <78618073+tomleglaunec@users.noreply.github.com>
2026-02-01 20:13:11 +0100

    fix: remove URL from stings.json and translations and remove unused error string (#66)

--- custom_components/linkytic/config_flow.py
+++ custom_components/linkytic/config_flow.py
@@ -34,6 +34,8 @@ from .const import (
     TICMODE_HISTORIC_LABEL,
     TICMODE_STANDARD,
     TICMODE_STANDARD_LABEL,
+    URL_HELP,
+    URL_ISSUES,
 )
 from .serial_reader import CannotConnect, CannotRead, linky_tic_tester
 
@@ -70,46 +72,46 @@ class LinkyTICConfigFlow(ConfigFlow, domain=DOMAIN):  # type:ignore
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the initial step."""
-        # No input
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
-            )
-        # Validate input
-        await self.async_set_unique_id(DOMAIN + "_" + user_input[SETUP_SERIAL])
-        self._abort_if_unique_id_configured()
-
-        # Search for serial/by-id, which SHOULD be a persistent name to serial interface.
-        _port = await self.hass.async_add_executor_job(
-            usb.get_serial_by_id, user_input[SETUP_SERIAL]
-        )
 
         errors = {}
-        title = user_input[SETUP_SERIAL]
-        try:
-            # Encapsulate the tester function, pyserial rfc2217 implementation have blocking calls.
-            await asyncio.to_thread(
-                linky_tic_tester,
-                device=_port,
-                std_mode=user_input[SETUP_TICMODE] == TICMODE_STANDARD,
+        if user_input is not None:
+            # Validate input
+            await self.async_set_unique_id(DOMAIN + "_" + user_input[SETUP_SERIAL])
+            self._abort_if_unique_id_configured()
+
+            # Search for serial/by-id, which SHOULD be a persistent name to serial interface.
+            _port = await self.hass.async_add_executor_job(
+                usb.get_serial_by_id, user_input[SETUP_SERIAL]
             )
-        except CannotConnect as cannot_connect:
-            _LOGGER.error("%s: can not connect: %s", title, cannot_connect)
-            errors["base"] = "cannot_connect"
-        except CannotRead as cannot_read:
-            _LOGGER.error(
-                "%s: can not read a line after connection: %s", title, cannot_read
-            )
-            errors["base"] = "cannot_read"
-        except Exception as exc:  # pylint: disable=broad-except
-            _LOGGER.exception("Unexpected exception: %s", exc)
-            errors["base"] = "unknown"
-        else:
-            user_input[SETUP_SERIAL] = _port
-            return self.async_create_entry(title=title, data=user_input)
+
+            title = user_input[SETUP_SERIAL]
+            try:
+                # Encapsulate the tester function, pyserial rfc2217 implementation have blocking calls.
+                await asyncio.to_thread(
+                    linky_tic_tester,
+                    device=_port,
+                    std_mode=user_input[SETUP_TICMODE] == TICMODE_STANDARD,
+                )
+            except CannotConnect as cannot_connect:
+                _LOGGER.error("%s: can not connect: %s", title, cannot_connect)
+                errors["base"] = "cannot_connect"
+            except CannotRead as cannot_read:
+                _LOGGER.error(
+                    "%s: can not read a line after connection: %s", title, cannot_read
+                )
+                errors["base"] = "cannot_read"
+            except Exception as exc:  # pylint: disable=broad-except
+                _LOGGER.exception("Unexpected exception: %s", exc)
+                errors["base"] = "unknown"
+            else:
+                user_input[SETUP_SERIAL] = _port
+                return self.async_create_entry(title=title, data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user",
+            data_schema=STEP_USER_DATA_SCHEMA,
+            errors=errors,
+            description_placeholders={"url_help": URL_HELP},
         )
 
     # async def async_step_usb(self, discovery_info: UsbServiceInfo) -> FlowResult:

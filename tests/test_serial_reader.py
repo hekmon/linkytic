873db07bb4bbb25f8abac9522a9c8c267232b66a
Tom Benezech <tom.leglaunec@gmail.com>
2026-09-10 17:53:42 +0200

    test: add serial reader tests

--- tests/test_serial_reader.py
+++ tests/test_serial_reader.py
@@ -2,18 +2,21 @@
 
 import asyncio
 import contextlib
+import logging
 import os
 import subprocess
 import tempfile
 from collections.abc import Callable, Iterator
 from typing import IO, Any
+from unittest.mock import MagicMock, patch
 
 import pytest
 import serialx
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-from custom_components.linkytic.const import DOMAIN
+from custom_components.linkytic import const
 from custom_components.linkytic.serial_reader import (
     Dataset,
     HistoricDataset,
@@ -21,25 +24,11 @@ from custom_components.linkytic.serial_reader import (
     LinkQualityIndicator,
     LinkyMeter,
     MalformatedDatasetException,
+    SerialNumberMismatch,
     StandardDataset,
     TICProtocol,
 )
 
-
-def patch_config_entry() -> ConfigEntry:
-    """Path a HA config entry."""
-    return ConfigEntry(
-        discovery_keys=None, #type: ignore[assignment]
-        data={},
-        domain=DOMAIN,
-        version=1,
-        minor_version=1,
-        options={},
-        source="",
-        subentries_data=[],
-        title="MockEntry",
-        unique_id="1"
-    )
 
 # Nicely borrowed from serialx
 @contextlib.contextmanager
@@ -146,11 +135,12 @@ def create_socat_pair() -> Iterator[
                     proc.stderr.close()
 
 
-@pytest.mark.parametrize("data,tag,value", 
+@pytest.mark.parametrize(
+    "data,tag,value",
     [
         (b"EAST\t003519702\t*", "EAST", "003519702"),
         (b"ADSC\t001122334455\t+", "ADSC", "001122334455"),
-    ]
+    ],
 )
 def test_standard_dataset_decode(data, tag, value):
     """Test the decoding of a standard dataset."""
@@ -161,11 +151,12 @@ def test_standard_dataset_decode(data, tag, value):
     assert dataset.value == value
 
 
-@pytest.mark.parametrize("data",
+@pytest.mark.parametrize(
+    "data",
     [
         (b"EAST\t003519702\t0"),
         (b"ADSC\t001122334455\t0"),
-    ]
+    ],
 )
 def test_standard_dataset_invalid_checksum(data):
     """Test that an invalid checksum raises an exception."""
@@ -173,11 +164,13 @@ def test_standard_dataset_invalid_checksum(data):
     with pytest.raises(InvalidChecksumException):
         StandardDataset.from_raw(data)
 
-@pytest.mark.parametrize("data",
+
+@pytest.mark.parametrize(
+    "data",
     [
         (b"EAST\t003519702\t0"),
         (b"ADSC\t001122334455\t0"),
-    ]
+    ],
 )
 def test_standard_dataset_malformed(data):
     """Test that a malformed dataset raises an exception."""
@@ -186,11 +179,13 @@ def test_standard_dataset_malformed(data):
     with pytest.raises(MalformatedDatasetException):
         StandardDataset.from_raw(data)
 
-@pytest.mark.parametrize("data,tag,value",
+
+@pytest.mark.parametrize(
+    "data,tag,value",
     [
         (b"ADCO 012345678910 E", "ADCO", "012345678910"),
         (b"BASE 123456789 8", "BASE", "123456789"),
-    ]
+    ],
 )
 def test_historic_dataset_decode(data, tag, value):
     """Test the decoding of a historic dataset."""
@@ -201,11 +196,13 @@ def test_historic_dataset_decode(data, tag, value):
     assert dataset.value == value
     assert dataset.timestamp is None
 
-@pytest.mark.parametrize("data",
+
+@pytest.mark.parametrize(
+    "data",
     [
         (b"ADCO 012345678910 0"),
         (b"BASE 123456789 0"),
-    ]
+    ],
 )
 def test_historic_dataset_invalid_checksum(data):
     """Test that an invalid checksum raises an exception."""
@@ -213,11 +210,13 @@ def test_historic_dataset_invalid_checksum(data):
     with pytest.raises(InvalidChecksumException):
         HistoricDataset.from_raw(data)
 
-@pytest.mark.parametrize("data",
+
+@pytest.mark.parametrize(
+    "data",
     [
         (b"ADCOx012345678910 0"),
         (b"BASE 123456789 0a"),
-    ]
+    ],
 )
 def test_historic_dataset_malformed(data):
     """Test that a malformed dataset raises an exception."""
@@ -237,45 +236,216 @@ def test_link_quality_indicator():
     assert lqi.get_value() == 0
 
 
-async def test_standard_tic_protocol():
+async def test_standard_tic_protocol(hass: HomeAssistant, config_entry: ConfigEntry):
     """Test standard TIC protocol."""
     meter = LinkyMeter()
     meter._mode_std = True
-    meter._config = patch_config_entry()
+    meter._config = config_entry
+    meter._hass = MagicMock(hass)
 
     with create_socat_pair() as (left_tty, right_tty, _, _):
         with open(left_tty, "wb") as writer:
-            
             transport, protocol = await serialx.create_serial_connection(
                 loop=asyncio.get_running_loop(),
                 protocol_factory=lambda: TICProtocol(meter, StandardDataset),
                 url=right_tty,
-                baudrate=9600
+                baudrate=9600,
             )
-            writer.write(b"\x02\x0AADSC\t001122334455\t+\x0D\x0AEAST\t003519702\t*\x0D\x03")
+            writer.write(
+                b"\x02\x0aADSC\t001122334455\t+\x0d\x0aEAST\t003519702\t*\x0d\x03"
+            )
             writer.flush()
             await meter._serial_number_read
 
-    assert meter._values == {"ADSC": Dataset("ADSC", "001122334455", None), "EAST": Dataset("EAST", "003519702", None)}
+    assert meter._values == {
+        "ADSC": Dataset("ADSC", "001122334455", None),
+        "EAST": Dataset("EAST", "003519702", None),
+    }
 
 
-async def test_historic_tic_protocol():
+async def test_historic_tic_protocol(hass: HomeAssistant, config_entry: ConfigEntry):
     """Test standard TIC protocol."""
     meter = LinkyMeter()
     meter._mode_std = False
-    meter._config = patch_config_entry()
+    meter._config = config_entry
+    meter._hass = MagicMock(hass)
 
     with create_socat_pair() as (left_tty, right_tty, _, _):
         with open(left_tty, "wb") as writer:
-            
             transport, protocol = await serialx.create_serial_connection(
                 loop=asyncio.get_running_loop(),
                 protocol_factory=lambda: TICProtocol(meter, HistoricDataset),
                 url=right_tty,
-                baudrate=9600
+                baudrate=9600,
             )
-            writer.write(b"\x02\x0AADCO 012345678910 E\x0D\x0ABASE 123456789 8\x0D\x03")
+            writer.write(b"\x02\x0aADCO 012345678910 E\x0d\x0aBASE 123456789 8\x0d\x03")
             writer.flush()
             await meter._serial_number_read
 
-    assert meter._values == {"ADCO": Dataset("ADCO", "012345678910", None), "BASE": Dataset("BASE", "123456789", None)}
+    assert meter._values == {
+        "ADCO": Dataset("ADCO", "012345678910", None),
+        "BASE": Dataset("BASE", "123456789", None),
+    }
+
+
+async def test_meter_from_config_no_port(
+    hass: HomeAssistant, config_entry: ConfigEntry
+):
+    """Test file not found error for device config."""
+
+    with pytest.raises(FileNotFoundError):
+        await LinkyMeter.connect_from_config(hass, config_entry)
+
+
+async def test_meter_from_config_timeout(hass: HomeAssistant):
+    """Test timeout error on connection."""
+
+    with (
+        create_socat_pair() as (tty, *_),
+        patch("custom_components.linkytic.serial_reader.CONNECTION_TIMEOUT", 0.01),
+    ):
+        config_entry = MockConfigEntry(
+            data={
+                const.SETUP_SERIAL: tty,
+                const.SETUP_TICMODE: const.TICMODE_STANDARD,
+            }
+        )
+        with pytest.raises(TimeoutError):
+            await LinkyMeter.connect_from_config(hass, config_entry)
+
+
+@pytest.mark.parametrize(
+    ("mode", "frame"),
+    (
+        (
+            const.TICMODE_STANDARD,
+            b"\x02\x0aADSC\t001122334455\t+\x0d\x0aEAST\t003519702\t*\x0d\x03",
+        ),
+        (
+            const.TICMODE_HISTORIC,
+            b"\x02\x0aADCO 001122334455 5\x0d\x0aBASE 123456789 8\x0d\x03",
+        ),
+    ),
+)
+async def test_meter_from_config(hass: HomeAssistant, mode, frame):
+    """Test S/N mismatch error on connection."""
+
+    hass = MagicMock(hass)
+
+    with (
+        create_socat_pair() as (input_tty, output_tty, _, kill_input),
+        patch("custom_components.linkytic.serial_reader.CONNECTION_TIMEOUT", 0.1),
+    ):
+        config_entry = MockConfigEntry(
+            data={
+                const.SETUP_SERIAL: output_tty,
+                const.SETUP_TICMODE: mode,
+            }
+        )
+        async with serialx.async_serial_for_url(input_tty, baudrate=9600) as writer:
+            async with asyncio.TaskGroup() as tg:
+
+                async def emulate_tic() -> None:
+                    while True:
+                        if not writer.is_open:
+                            return
+                        await writer.write(frame)
+                        await asyncio.sleep(0.1)
+
+                tg.create_task(emulate_tic())
+
+                meter = await LinkyMeter.connect_from_config(hass, config_entry)
+                assert meter.serial_number == "001122334455"
+                kill_input()
+                await meter.disconnect(None)
+
+
+@pytest.mark.parametrize("realtime", (True, False))
+def test_update_callback(realtime):
+    """Test registering callback, and update."""
+
+    data = []
+
+    def callback(realtime: bool):
+        data.append(realtime)
+
+    meter = LinkyMeter()
+    meter._config = MockConfigEntry(options={const.OPTIONS_REALTIME: realtime})
+    meter.register_update_callback("DEMO", callback)
+
+    meter._handle_new_values({"DEMO": Dataset("DEMO", "test", None)})
+
+    assert data[0] is realtime
+
+
+def test_historic_short_frame_callback():
+    """Test historic short frame handler."""
+
+    frame = {
+        "ADS": Dataset("", "", None),
+        "ADIR1": Dataset("", "", None),
+        "ADIR2": Dataset("", "", None),
+        "ADIR3": Dataset("", "", None),
+        "IINST1": Dataset("", "", None),
+        "IINST2": Dataset("", "", None),
+        "IINST3": Dataset("", "", None),
+    }
+
+    data = []
+
+    def callback(realtime: bool):
+        data.append(realtime)
+
+    meter = LinkyMeter()
+    meter._config = MockConfigEntry(options={const.OPTIONS_REALTIME: False})
+
+    for tag in frame:
+        meter.register_update_callback(tag, callback)
+
+    meter._handle_new_values(frame)
+
+    assert data == [True] * frame.__len__()
+
+    data.clear()
+    # Test new values not in short frame
+
+    meter._handle_new_values({"ADS": Dataset("", "", "")})
+
+    assert data == [False]
+
+
+@pytest.mark.parametrize(
+    ("mode", "frame"),
+    (
+        (const.TICMODE_HISTORIC, [Dataset("ADCO", "2", None)]),
+        (const.TICMODE_STANDARD, [Dataset("ADSC", "2", None)]),
+    ),
+)
+def test_serial_number_mistmatch(mode, frame, caplog):
+    """Test detection of wrong serial number."""
+
+    meter = LinkyMeter()
+    meter._mode_std = mode == const.TICMODE_STANDARD
+    meter._serial_number_read.set_result("1")
+    meter._serial_number = "1"
+
+    with caplog.at_level(logging.WARNING):
+        meter.frame_received(frame)
+    assert "different meter S/N" in caplog.text
+
+
+@pytest.fixture(name="config_entry")
+def config_entry_fixture() -> MockConfigEntry:
+    """Fixture of a config entry"""
+    return MockConfigEntry(
+        version=2,
+        minor_version=1,
+        domain=const.DOMAIN,
+        data={
+            const.SETUP_SERIAL: "/dev/ttyUSBX",
+            const.SETUP_TICMODE: const.TICMODE_STANDARD,
+            const.SETUP_PRODUCER: False,
+            const.SETUP_THREEPHASE: False,
+        },
+        options={const.OPTIONS_REALTIME: False},
+    )
